f75cc2faed138b1f80098fe8ceac4233b5470e7e
nonLinearStiffness <jtorcatoa@gmail.com>
2026-02-14 08:42:40 +0000

    Update model viewer.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default defineConfig({
-<<<<<<< HEAD
   base: '/modelViewerBIM/',
-=======
-  base: '/arJSLocationBasedBIM/',
->>>>>>> 084098e165153fa32532065f878390a8a990f949
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, './certs/cert.key')),
